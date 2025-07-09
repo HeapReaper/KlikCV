ca85d3814d5f7091736863de44cd2ba2e5c8b6c8
AeroBytesNL <kelvin@aerobytes.nl>
2025-07-09 19:54:05 +0200

    Added author and icons to blog show

--- inertia/pages/blog/show.tsx
+++ inertia/pages/blog/show.tsx
@@ -19,10 +19,29 @@ export default function BlogShowPage({ html, article }: Props) {
       <DefaultLayout>
         <div className="pt-4 flex flex-col mt-8 p-3 max-w-3xl mx-auto">
           <h1 className="text-4xl font-bold mb-4">{article.title}</h1>
-          <div className="flex items-center gap-4 text-sm text-gray-500 mb-4">
-            <p>{article.author}</p>
-            <time>{article.publishedAt}</time>
+          <div className="flex items-center gap-4 text-sm text-gray-500 mb-4 mt-3">
+            <div className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"  className="size-6 fill-orange-500">
+                <path fill-rule="evenodd"  d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd"/>
+              </svg>
+              <p>
+                {article.author}
+              </p>
+            </div>
+
+            <div className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-orange-500">
+                <path fill-rule="evenodd"
+                      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z"
+                      clip-rule="evenodd"/>
+              </svg>
+              <time>
+                {article.publishedAt}
+              </time>
+            </div>
+
           </div>
+
           <div
             className="prose prose-neutral text-black max-w-none marker:text-black"
             dangerouslySetInnerHTML={{ __html: html }}
