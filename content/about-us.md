b7135cef635c2a8c154220bf2b676b3c73f55939
AeroBytesNL <kelvin@aerobytes.nl>
2025-07-09 19:06:02 +0200

    Added markup for privacy and about us

---
+++ content/about-us.md
@@ -0,0 +1,2 @@
+# Over ons
+Hi
--- start/routes.ts
+++ start/routes.ts
@@ -1,4 +1,5 @@
 import router from '@adonisjs/core/services/router'
+import { loadMd } from "../utils/marked.js";
 
 router.on('/').renderInertia('home')
 
@@ -6,6 +7,10 @@ router.on('/').renderInertia('home')
 router.on('/cv/bouw').renderInertia('builder/create')
 router.on('/blog').renderInertia('blog/page')
 router.on('/faq').renderInertia('faq/page')
-router.on('/over-ons').renderInertia('about-us/page')
+router.on('/over-ons').renderInertia('about-us/page', {
+  html: await loadMd('about-us.md')
+})
 router.on('/contact').renderInertia('contact/page')
-router.on('/privacy').renderInertia('privacy/page')
+router.on('/privacy').renderInertia('privacy/page', {
+  html: await loadMd('privacy.md')
+})
