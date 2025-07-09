1bbc23635776c725082f61c872e01063db2c9cb0
AeroBytesNL <kelvin@aerobytes.nl>
2025-07-09 19:52:46 +0200

    Add author information to blog articles

--- inertia/pages/blog/show.tsx
+++ inertia/pages/blog/show.tsx
@@ -5,6 +5,7 @@ interface Props {
   html: string;
   article: {
     title: string;
+    author: string;
     slug: string;
     publishedAt: string;
     excerpt: string;
@@ -18,7 +19,10 @@ export default function BlogShowPage({ html, article }: Props) {
       <DefaultLayout>
         <div className="pt-4 flex flex-col mt-8 p-3 max-w-3xl mx-auto">
           <h1 className="text-4xl font-bold mb-4">{article.title}</h1>
-          <time className="text-sm text-gray-500 mb-4">{article.publishedAt}</time>
+          <div className="flex items-center gap-4 text-sm text-gray-500 mb-4">
+            <p>{article.author}</p>
+            <time>{article.publishedAt}</time>
+          </div>
           <div
             className="prose prose-neutral text-black max-w-none marker:text-black"
             dangerouslySetInnerHTML={{ __html: html }}
