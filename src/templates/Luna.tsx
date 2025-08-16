c9b6ec2b04ac9408b1806f55026ccd303ad9a29b
AeroBytesNL <kelvin@aerobytes.nl>
2025-08-16 10:38:38 +0200

    Improved color switcher

--- src/templates/Luna.tsx
+++ src/templates/Luna.tsx
@@ -6,61 +6,62 @@ export default function Luna({ name, email, phone, city, birthdate, aboutMeDescr
     <>
       <style>{`:root { --primary-color: ${primaryColor}; }`}</style>
 
+
       <header role="banner" className="pb-4">
         <h1 className="text-6xl font-bold mt-3">
           {name || "John Doe"}
         </h1>
-        <h1 className="text-4xl" style="color: var(--primary-color)">
+        <h1 className="text-4xl text-[var(--primary-color)]">
           Software Engineer
         </h1>
         <address className="not-italic mt-2 text-gray-600 space-y-1 text-base">
           <div className="grid grid-cols-3 gap-4">
             <p className="flex items-center space-x-2">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                 className="size-4 mr-1" style="color: var(--primary-color)">
+                   className="size-4 mr-1 text-[var(--primary-color)]">
                 <path fillRule="evenodd"
                   d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
                   clipRule="evenodd"/>
               </svg>
               <a href="tel:+31 6 12345678"
-                 className=" hover:underline focus:outline-2 focus:outline-blue-600">
+                 className=" hover:underline focus:outline-2 focus:outline-[var(--primary-color)]">
                 {phone || "06-123456780"}
               </a>
             </p>
 
             <p className="flex items-center space-x-2">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                   className="size-4 mr-1" style="color: var(--primary-color)">
+                   className="size-4 mr-1 text-[var(--primary-color)]">
                 <path
                   d="M19.5 22.5a3 3 0 0 0 3-3v-8.174l-6.879 4.022 3.485 1.876a.75.75 0 1 1-.712 1.321l-5.683-3.06a1.5 1.5 0 0 0-1.422 0l-5.683 3.06a.75.75 0 0 1-.712-1.32l3.485-1.877L1.5 11.326V19.5a3 3 0 0 0 3 3h15Z"/>
                 <path
                   d="M1.5 9.589v-.745a3 3 0 0 1 1.578-2.642l7.5-4.038a3 3 0 0 1 2.844 0l7.5 4.038A3 3 0 0 1 22.5 8.844v.745l-8.426 4.926-.652-.351a3 3 0 0 0-2.844 0l-.652.351L1.5 9.589Z"/>
               </svg>
               <a href="mailto:john.doe@example.com"
-                 className=" hover:underline focus:outline-2 focus:outline-blue-600">
+                 className=" hover:underline focus:outline-2 focus:outline-[var(--primary-color)]">
                 {email || "john.doe@example.com"}
               </a>
             </p>
 
             <p className="flex items-center space-x-2">
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-1" fill="currentColor" style="color: var(--primary-color)"
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-[var(--primary-color)] mr-1" fill="currentColor"
                    viewBox="0 0 24 24">
                 <path
                   d="M19 0h-14a5 5 0 00-5 5v14a5 5 0 005 5h14a5 5 0 005-5v-14a5 5 0 00-5-5zm-11.667 19h-3v-9h3zm-1.5-10.271a1.726 1.726 0 110-3.451 1.726 1.726 0 010 3.451zm13.167 10.271h-3v-4.671c0-1.115-.021-2.55-1.554-2.55-1.557 0-1.794 1.216-1.794 2.472v4.749h-3v-9h2.885v1.228h.041a3.165 3.165 0 012.847-1.563c3.044 0 3.607 2.005 3.607 4.609z"/>
               </svg>
               <a href="https://nl.linkedin.com/idk" target="_blank" rel="noopener noreferrer"
-                 className=" hover:underline focus:outline-2 focus:outline-blue-600">
+                 className=" hover:underline focus:outline-2 focus:outline-[var(--primary-color)]">
                 nl.linkedin.com/idk
               </a>
             </p>
 
             <p className="flex items-center space-x-2">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 mr-1" style="color: var(--primary-color)">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 mr-1 text-[var(--primary-color)]">
                 <path fillRule="evenodd" d="M12 2a1 1 0 0 1 1 1v2h2a1 1 0 0 1 1 1v2h-8V6a1 1 0 0 1 1-1h2V3a1 1 0 0 1 1-1zm-7 7h14a1 1 0 0 1 1 1v7a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3v-7a1 1 0 0 1 1-1zm3 4a1 1 0 1 0 0 2h8a1 1 0 1 0 0-2H8z" clipRule="evenodd"/>
               </svg>
 
               <a href="https://github.com/johndoe" target="_blank" rel="noopener noreferrer"
-                 className=" hover:underline focus:outline-2 focus:outline-blue-600">
+                 className=" hover:underline focus:outline-2 focus:outline-[var(--primary-color)]">
                 {birthdate || "01-01-2000"}
               </a>
             </p>
@@ -82,7 +83,7 @@ export default function Luna({ name, email, phone, city, birthdate, aboutMeDescr
 
             <p className="flex items-center space-x-2">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                   className="size-5 mr-2" style="color: var(--primary-color)">
+                   className="size-5 mr-2 text-[var(--primary-color)]">
                 <path
                   d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z"/>
                 <path
@@ -103,7 +104,7 @@ export default function Luna({ name, email, phone, city, birthdate, aboutMeDescr
               <img
                 src="https://placehold.co/400"
                 alt={name || "Profielfoto"}
-                className="w-40 h-40 rounded-full object-cover border-4 border-blue-500 shadow-lg"
+                className="w-40 h-40 rounded-full object-cover border-4 border-[var(--primary-color)] shadow-lg"
               />
             </div>
 
@@ -120,12 +121,12 @@ export default function Luna({ name, email, phone, city, birthdate, aboutMeDescr
             <h3 id="functie1-heading" className="text-2xl text-black mb-1">
               Frontend Developer
             </h3>
-            <h3 id="functie1-heading" className="text-xl text-blue-700 mb-1">
+            <h3 id="functie1-heading" className="text-xl text-[var(--primary-color)] mb-1">
               Tech Corp
             </h3>
             <p className="flex items-center space-x-2">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                   className="size-4 mr-1 text-blue-700">
+                   className="size-4 mr-1 text-[var(--primary-color)]">
                 <path
                   d="M12.75 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM7.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM8.25 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM9.75 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM10.5 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM12.75 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM14.25 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 13.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"/>
                 <path fill-rule="evenodd"
@@ -135,7 +136,7 @@ export default function Luna({ name, email, phone, city, birthdate, aboutMeDescr
               2020-01 - 2023-06
 
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                   className="size-4 mr-1 text-blue-700">
+                   className="size-4 mr-1 text-[var(--primary-color)]">
                 <path fill-rule="evenodd"
                       d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
                       clip-rule="evenodd"/>
@@ -154,10 +155,10 @@ export default function Luna({ name, email, phone, city, birthdate, aboutMeDescr
 
           <article className="mb-6">
             <h3 className="text-2xl text-black mb-1">BSc Computer Science</h3>
-            <h3 className="text-xl text-blue-700 mb-1">University of Amsterdam</h3>
+            <h3 className="text-xl text-[var(--primary-color)] mb-1">University of Amsterdam</h3>
             <p className="flex items-center space-x-2">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                   className="size-4 mr-1 text-blue-700">
+                   className="size-4 mr-1 text-[var(--primary-color)]">
                 <path
                   d="M12.75 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM7.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM8.25 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM9.75 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM10.5 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM12.75 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM14.25 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 13.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"/>
                 <path fill-rule="evenodd"
@@ -167,7 +168,7 @@ export default function Luna({ name, email, phone, city, birthdate, aboutMeDescr
               2010-09 - 2014-06
 
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                   className="size-4 mr-1 text-blue-700">
+                   className="size-4 mr-1 text-[var(--primary-color)]">
                 <path fill-rule="evenodd"
                       d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
                       clip-rule="evenodd"/>
