8ffc6588d828f6f36ab50195ef2d7dae88d369f2
AeroBytesNL <kelvin@aerobytes.nl>
2025-08-17 11:05:53 +0200

    Improved desktop experience

--- src/pages/CvBuilder.tsx
+++ src/pages/CvBuilder.tsx
@@ -20,8 +20,8 @@ import { useCvState } from '../hooks/useCvState';
 
 // Templates
 import Luna from '../templates/Luna';
-import TextInput from "../components/inputs/Text.tsx";
-import SkillLevelSelect from "../components/select/LevelSelect.tsx";
+import TextInput from '../components/inputs/Text';
+import SkillLevelSelect from '../components/select/LevelSelect';
 
 export default function CvBuilder() {
   const [cvData, setCvData] = useCvState({
@@ -101,6 +101,7 @@ export default function CvBuilder() {
           {/* Theme */}
           <div className="p-2 space-y-2 rounded-2xl border-2 border-orange-500">
             <h4 className="text-2xl">Thema</h4>
+
             <div className="space-y-4 p-4 bg-white rounded-lg shadow-md w-full max-w-sm">
               <div className="flex gap-4">
                 <ColorPicker label="Primary kleur" value={primaryColor} onChange={setPrimaryColor} />
@@ -113,16 +114,20 @@ export default function CvBuilder() {
           {/* Personal info */}
           <div className="p-2 space-y-2 rounded-2xl border-2 border-orange-500">
             <h4 className="text-2xl">Persoonlijke informatie</h4>
-            <FullName value={cvData.fullName} onChange={value => updateCvData('fullName', value)} />
-            <Email value={cvData.email} onChange={value => updateCvData('email', value)} />
-            <Phone value={cvData.phone} onChange={value => updateCvData('phone', value)} />
-            <City value={cvData.city} onChange={value => updateCvData('city', value)} />
-            <Birthdate value={cvData.birthdate} onChange={value => updateCvData('birthdate', value)} />
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+              <FullName value={cvData.fullName} onChange={value => updateCvData('fullName', value)} />
+              <Email value={cvData.email} onChange={value => updateCvData('email', value)} />
+              <Phone value={cvData.phone} onChange={value => updateCvData('phone', value)} />
+              <City value={cvData.city} onChange={value => updateCvData('city', value)} />
+              <Birthdate value={cvData.birthdate} onChange={value => updateCvData('birthdate', value)} />
+            </div>
           </div>
 
           {/* About me */}
           <div className="p-2 space-y-2 rounded-2xl border-2 border-orange-500">
             <h4 className="text-2xl">Profiel</h4>
+
             <TextInput label="Gewenste functie" placeholder="Gewenste functie" value={cvData.preferredFunction} onChange={value => updateCvData('preferredFunction', value)} />
             <AboutMeDescription value={cvData.aboutMeDescription} onChange={html => updateCvData('aboutMeDescription', html)} />
           </div>
@@ -137,28 +142,33 @@ export default function CvBuilder() {
                   <RemoveButton onClick={() => removeListItem('educations', index)} />
                 </div>
                 <TextInput id="title" label="Opleiding" placeholder="Naam" value={education.name} onChange={value => updateListItem('educations', index, 'name', value)} />
-                <div className="flex space-x-4">
-                  <TextInput id="institution" label="Instituut" placeholder="School naam.." value={education.institution} onChange={value => updateListItem('educations', index, 'institution', value)} />
-                  <TextInput id="Place" label="Plaats" placeholder="Plaats" value={education.place} onChange={value => updateListItem('educations', index, 'place', value)} />
-                </div>
-                <div className="flex space-x-4 gap-2">
-                  <div>
-                    <p className="font-medium text-gray-700">Startdatum</p>
-                    <div className="flex space-x-4 gap-2">
-                      <MonthSelect value={education.startMonth} onChange={value => updateListItem('educations', index, 'startMonth', value)} />
-                      <YearSelect value={education.startYear} onChange={value => updateListItem('educations', index, 'startYear', value)} />
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                  <div className="flex space-x-4">
+                    <TextInput id="institution" label="Instituut" placeholder="School naam" value={education.institution} onChange={value => updateListItem('educations', index, 'institution', value)} />
+                    <TextInput id="Place" label="Plaats" placeholder="Plaats" value={education.place} onChange={value => updateListItem('educations', index, 'place', value)} />
+                  </div>
+
+                  <div className="flex space-x-4 gap-2">
+                    <div>
+                      <p className="font-medium text-gray-700">Startdatum</p>
+                      <div className="flex space-x-4 gap-2">
+                        <MonthSelect value={education.startMonth} onChange={value => updateListItem('educations', index, 'startMonth', value)} />
+                        <YearSelect value={education.startYear} onChange={value => updateListItem('educations', index, 'startYear', value)} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="flex items-center justify-between">
+                        <p className="font-medium text-gray-700">Einddatum</p>
+                        <CheckBox label="Huidig" checked={education.current} onChange={checked => updateListItem('educations', index, 'current', checked)} />
+                      </div>
+                      <div className="flex space-x-4 gap-2">
+                        <MonthSelect value={education.endMonth} onChange={value => updateListItem('educations', index, 'endMonth', value)} />
+                        <YearSelect value={education.endYear} onChange={value => updateListItem('educations', index, 'endYear', value)} />
+                      </div>
                     </div>
                   </div>
-                  <div>
-                    <div className="flex items-center justify-between">
-                      <p className="font-medium text-gray-700">Einddatum</p>
-                      <CheckBox label="Huidig" checked={education.current} onChange={checked => updateListItem('educations', index, 'current', checked)} />
-                    </div>
-                    <div className="flex space-x-4 gap-2">
-                      <MonthSelect value={education.endMonth} onChange={value => updateListItem('educations', index, 'endMonth', value)} />
-                      <YearSelect value={education.endYear} onChange={value => updateListItem('educations', index, 'endYear', value)} />
-                    </div>
-                  </div>
+
                 </div>
                 <div>
                   <p className="font-medium text-gray-700">Omschrijving</p>
@@ -177,30 +187,36 @@ export default function CvBuilder() {
                   <AddButton onClick={() => addListItem('workExperiences', { jobTitle: '', employer: '', place: '', startMonth: '', startYear: '', endMonth: '', endYear: '', current: false, description: '' })} />
                   <RemoveButton onClick={() => removeListItem('workExperiences', index)} />
                 </div>
+
                 <TextInput id={`function-${index}`} label="Functie" placeholder="Functie" value={experience.jobTitle} onChange={value => updateListItem('workExperiences', index, 'jobTitle', value)} />
-                <div className="flex space-x-4">
-                  <TextInput id={`employer-${index}`} label="Werkgever" placeholder="Werkgever" value={experience.employer} onChange={value => updateListItem('workExperiences', index, 'employer', value)} />
-                  <TextInput id={`place-${index}`} label="Plaats" placeholder="Plaats" value={experience.place} onChange={value => updateListItem('workExperiences', index, 'place', value)} />
-                </div>
-                <div className="flex space-x-4 gap-2">
-                  <div>
-                    <p className="font-medium text-gray-700">Startdatum</p>
-                    <div className="flex space-x-4 gap-2">
-                      <MonthSelect value={experience.startMonth} onChange={month => updateListItem('workExperiences', index, 'startMonth', month)} />
-                      <YearSelect value={experience.startYear} onChange={year => updateListItem('workExperiences', index, 'startYear', Number(year))} />
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+
+                  <div className="flex space-x-4">
+                    <TextInput id={`employer-${index}`} label="Werkgever" placeholder="Werkgever" value={experience.employer} onChange={value => updateListItem('workExperiences', index, 'employer', value)} />
+                    <TextInput id={`place-${index}`} label="Plaats" placeholder="Plaats" value={experience.place} onChange={value => updateListItem('workExperiences', index, 'place', value)} />
+                  </div>
+                  <div className="flex space-x-4 gap-2">
+                    <div>
+                      <p className="font-medium text-gray-700">Startdatum</p>
+                      <div className="flex space-x-4 gap-2">
+                        <MonthSelect value={experience.startMonth} onChange={month => updateListItem('workExperiences', index, 'startMonth', month)} />
+                        <YearSelect value={experience.startYear} onChange={year => updateListItem('workExperiences', index, 'startYear', Number(year))} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="flex items-center justify-between">
+                        <p className="font-medium text-gray-700">Einddatum</p>
+                        <CheckBox label="Huidig" checked={experience.current} onChange={checked => updateListItem('workExperiences', index, 'current', checked)} />
+                      </div>
+                      <div className="flex space-x-4 gap-2">
+                        <MonthSelect value={experience.endMonth} onChange={month => updateListItem('workExperiences', index, 'endMonth', month)} />
+                        <YearSelect value={experience.endYear} onChange={year => updateListItem('workExperiences', index, 'endYear', year)} />
+                      </div>
                     </div>
                   </div>
-                  <div>
-                    <div className="flex items-center justify-between">
-                      <p className="font-medium text-gray-700">Einddatum</p>
-                      <CheckBox label="Huidig" checked={experience.current} onChange={checked => updateListItem('workExperiences', index, 'current', checked)} />
-                    </div>
-                    <div className="flex space-x-4 gap-2">
-                      <MonthSelect value={experience.endMonth} onChange={month => updateListItem('workExperiences', index, 'endMonth', month)} />
-                      <YearSelect value={experience.endYear} onChange={year => updateListItem('workExperiences', index, 'endYear', year)} />
-                    </div>
-                  </div>
                 </div>
+
                 <div>
                   <p className="font-medium text-gray-700">Omschrijving</p>
                   <RichTextEditor value={experience.description} onChange={val => updateListItem('workExperiences', index, 'description', val)} />
@@ -212,31 +228,38 @@ export default function CvBuilder() {
           {/* Skills */}
           <div className="p-2 space-y-2 rounded-2xl border-2 border-orange-500">
             <h4 className="text-2xl">Skills</h4>
-            {cvData.skills.map((skill: any, index: number) => (
-              <div key={index} className="p-2 relative flex space-y-4 space-x-4 rounded-2xl border-2 border-orange-500">
-                <div className="absolute flex gap-2 -top-4 right-1 space-x-2">
-                  <AddButton onClick={() => addListItem('skills', { skill: '', level: '' })} />
-                  <RemoveButton onClick={() => removeListItem('skills', index)} />
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+              {cvData.skills.map((skill: any, index: number) => (
+                <div key={index} className="p-2 relative flex space-y-4 space-x-4 rounded-2xl border-2 border-orange-500">
+                  <div className="absolute flex gap-2 -top-4 right-1 space-x-2">
+                    <AddButton onClick={() => addListItem('skills', { skill: '', level: '' })} />
+                    <RemoveButton onClick={() => removeListItem('skills', index)} />
+                  </div>
+                  <TextInput id="skill[]" label="Naam" placeholder="Project management" value={skill.skill} onChange={value => updateListItem('skills', index, 'skill', value)} />
+                  <SkillLevelSelect value={skill.level} onChange={value => updateListItem('skills', index, 'level', value)} />
                 </div>
-                <TextInput id="skill[]" label="Naam" placeholder="Project management" value={skill.skill} onChange={value => updateListItem('skills', index, 'skill', value)} />
-                <SkillLevelSelect value={skill.level} onChange={value => updateListItem('skills', index, 'level', value)} />
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
 
           {/* Languages */}
           <div className="p-2 space-y-2 rounded-2xl border-2 border-orange-500">
             <h4 className="text-2xl">Talen</h4>
-            {cvData.languages.map((lang: any, index: number) => (
-              <div key={index} className="p-2 relative flex space-y-4 space-x-4 rounded-2xl border-2 border-orange-500">
-                <div className="absolute flex gap-2 -top-4 right-1 space-x-2">
-                  <AddButton onClick={() => addListItem('languages', { language: '', level: '' })} />
-                  <RemoveButton onClick={() => removeListItem('languages', index)} />
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+              {cvData.languages.map((lang: any, index: number) => (
+                <div key={index} className="p-2 relative flex space-y-4 space-x-4 rounded-2xl border-2 border-orange-500">
+                  <div className="absolute flex gap-2 -top-4 right-1 space-x-2">
+                    <AddButton onClick={() => addListItem('languages', { language: '', level: '' })} />
+                    <RemoveButton onClick={() => removeListItem('languages', index)} />
+                  </div>
+                  <LanguageSelect value={lang.language} onChange={value => updateListItem('languages', index, 'language', value)} />
+                  <LanguageLevelSelect value={lang.level} onChange={value => updateListItem('languages', index, 'level', value)} />
                 </div>
-                <LanguageSelect value={lang.language} onChange={value => updateListItem('languages', index, 'language', value)} />
-                <LanguageLevelSelect value={lang.level} onChange={value => updateListItem('languages', index, 'level', value)} />
-              </div>
-            ))}
+              ))}
+            </div>
+
           </div>
 
           <button onClick={() => exportToPdf('Luna', 'cv.pdf')} className="mb-4 px-6 py-3 bg-orange-500 text-white rounded-lg hover:bg-orange-600 transition-colors">
