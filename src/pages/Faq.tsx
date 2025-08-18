7726802daea57e3782f7031b0899e6236d54f652
AeroBytesNL <kelvin@aerobytes.nl>
2025-08-18 21:38:02 +0200

    Updated max width

--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -2,9 +2,9 @@ import FAQ from '../components/core/Faq';
 
 export default function Faq() {
   return (
-    <div className="pt-4 h-full flex flex-col">
-      <div className="container mx-auto px-4">
-        <h1 className="text-5xl font-bold text-center text-orange-500 mb-8">
+    <div className="pt-4 h-full flex flex-col items-center">
+      <div className="container max-w-4xl px-4">
+        <h1 className="text-5xl font-bold text-orange-500 mb-8">
           Veelgestelde vragen
         </h1>
         <FAQ />
--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -12,9 +12,9 @@ export default function Privacy() {
 
   return (
     <>
-      <div className="pt-4 flex flex-col mt-8">
+      <div className="pt-4 flex items-center flex-col mt-8">
         <div
-          className="prose prose-neutral text-black max-w-none marker:text-black prose-h1:text-orange-500"
+          className="prose prose-neutral text-black max-w-4xl marker:text-black prose-h1:text-orange-500"
           dangerouslySetInnerHTML={{ __html: content }}
         />
       </div>
