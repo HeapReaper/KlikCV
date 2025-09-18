e2d97ca144ccbb274067d9454bf945a97ebcf459
HeapReaper <kelvin@aerobytes.nl>
2025-09-18 10:30:16 +0200

    Updated path chromium

--- src/app/api/generate-pdf/route.ts
+++ src/app/api/generate-pdf/route.ts
@@ -10,8 +10,10 @@ export async function POST(req: Request) {
 
   const browser = await puppeteer.launch({
     headless: true,
+    executablePath: '/usr/bin/chromium',
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
+
   const page = await browser.newPage();
 
   await page.setContent(html, { waitUntil: "networkidle0" });
