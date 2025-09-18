87d380ef5c3cf095769517b05535d39bbd3f0f4d
HeapReaper <kelvin@aerobytes.nl>
2025-09-18 14:17:41 +0200

    Small fixes

--- src/components/selects/templateSelect.tsx
+++ src/components/selects/templateSelect.tsx
@@ -13,7 +13,7 @@ export default function TemplateSelect({ value, onChange }: InputType) {
         name="template"
         value={value}
         onChange={(e) => onChange((e.currentTarget as unknown as HTMLInputElement).value)}
-        className="bg-white border border-solid border-orange-500 text-gray-700 dark:text-white dark:bg-gray-950 text-sm rounded-lg focus:ring-2 focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5"
+        className="bg-white border border-solid border-orange-500 text-gray-700 dark:text-white dark:bg-gray-950 text-sm rounded-lg focus:ring-2 focus:ring-orange-500 focus:border-orange-500  p-2.5"
       >
         {Object.keys(templateNames).map((key: string) => (
           <option key={key} value={key}>
