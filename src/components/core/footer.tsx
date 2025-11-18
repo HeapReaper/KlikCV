d29f4836deaab799caf1bb48ae827775d46fe6bc
HeapReaper <kelvin@aerobytes.nl>
2025-11-18 16:52:51 +0100

    Updated footer

--- src/components/core/footer.tsx
+++ src/components/core/footer.tsx
@@ -3,11 +3,8 @@ export default function Footer() {
     <footer className="mt-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 flex flex-col md:flex-row justify-between items-center text-gray-700 dark:text-white text-sm">
         <div className="mb-4 md:mb-0 text-gray-700 dark:text-white">
-          &copy; {new Date().getFullYear()}
-          <a href="https://heapreaper.nl" className=" ms-1 underline decoration-orange-500 hover:text-orange-600" target="_blank">
-            KlikCV
-          </a>.
-          Alle rechten voorbehouden.
+          <p className=" text-gray-300">
+            <p>&copy; {new Date().getFullYear()} <span className="underline decoration-purple-700 hover:text-white"><a href="https://klikbit.nl" target="_blank">KlikBit</a></span></p>          </p>
         </div>
         <div className="space-x-4 text-gray-700 dark:text-white">
           <a href="/privacy" className="hover:text-orange-600">
